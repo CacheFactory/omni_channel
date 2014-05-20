5a30086eefb5059a9a9535086c2cdb06866dc9f2
Edward Anderson <ejanderson4@gmail.com>
2014-05-20 10:14:50 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OmniChannel::Application.config.secret_key_base = '5c83345a45b913de43fa3b494dc462fa88e60266db84070aa97bf3b3327f9c90b2dcb351518a8b4ddaebda3b45eaa6845dc115d454e81480de2e1ea243484c45'
