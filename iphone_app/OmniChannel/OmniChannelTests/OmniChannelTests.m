6b91b220da75f93a2c1e85cb809c356f4114b2f4
Edward Anderson <ejanderson4@gmail.com>
2014-05-21 14:56:57 -0700

    adding iphone app project

---
+++ iphone_app/OmniChannel/OmniChannelTests/OmniChannelTests.m
@@ -0,0 +1,34 @@
+//
+//  OmniChannelTests.m
+//  OmniChannelTests
+//
+//  Created by Edward anderson on 5/21/14.
+//  Copyright (c) 2014 Edward anderson. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OmniChannelTests : XCTestCase
+
+@end
+
+@implementation OmniChannelTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
